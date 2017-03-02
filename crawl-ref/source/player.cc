da8b8941c1ba0258c9525d5327f8a93410a68857
Doesnt <doesntalt@gmail.com>
2017-03-02 13:25:15 -0600

    Implement spell library

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -5043,6 +5043,7 @@ player::player()
     runes.reset();
     obtainable_runes = 15;
 
+    spell_library.reset();
     spells.init(SPELL_NO_SPELL);
     old_vehumet_gifts.clear();
     spell_no        = 0;
